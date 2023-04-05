240bf13283460e16d78b46ffb8f5fb2e07c76d69
dnguyen <eonho6582@gmail.com>
2023-04-05 14:38:38 +0700

    v2

--- home/tests.py
+++ home/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
-class simpleTest(SimpleTestCase):
+class simpleTests(SimpleTestCase):
     def test_home_page_status(self):
         reponse = self.client.get('/')
         self.assertEqual(reponse.status_code, 200)
